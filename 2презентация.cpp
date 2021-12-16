0153bad0e0585220dbf51793ed9ee71bb8f5cfc5
amigo228 <61027311+amigo228@users.noreply.github.com>
2021-12-16 18:00:23 +0200

    Create 2презентация.cpp

---
+++ 2презентация.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int a, b, c;
+  cin>>a>>b>>c;
+  int s;
+  double arif, d;
+  s = a + b + c;
+  arif = s / 3;
+  d = a * b * c;
+  cout<<d<<endl;
+  cout<<arif<<endl;
+  cout<<s<<endl;
+return 0;
+}
